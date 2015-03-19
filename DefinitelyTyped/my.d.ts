7a83d34be0900861368bfe7178fc98fe240c5a2e
Alex Hyrenko <Satanpit@ukrnet.private>
2015-03-19 20:39:11 +0200

    start web components experiments

---
+++ DefinitelyTyped/my.d.ts
@@ -0,0 +1,34 @@
+interface Document {
+    /**
+     * Register custom element
+     */
+    registerElement(name: string, prototype?: Object): HTMLElement;
+}
+
+interface HTMLElement {
+    /**
+     * Set shadow DOM in this element
+     */
+    createShadowRoot(): DocumentFragment
+}
+
+interface NotifierNotify {
+    new(): Object;
+    (): any;
+
+    prototype: {
+        notify(): any;
+        performChange(): any;
+    }
+}
+
+interface Object {
+
+    observe(beingObserved: any, callback: (update: any, changes?: any) => any) : void;
+
+    getNotifier(beingObserved: any): NotifierNotify
+
+    deliverChangeRecords(callback: Function): void
+
+    unobserve(observer: Function, callback?: Function): void
+}
